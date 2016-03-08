e19876dc751812253975ed799506a75676606fea
suyu34 <joooye1991@gmail.com>
2016-03-08 13:57:56 +0800

    Update locales

--- gulp/filter.js
+++ gulp/filter.js
@@ -1,15 +1,22 @@
 
 var through = require('through2')
+var path = require('path')
 
 // exporting the plugin main function
-module.exports = function (description) {
+module.exports = function (description, descriptionAs) {
   return through.obj(function (file, ence, next) {
     if (!file.isBuffer()) return next()
 
+    var extname = path.extname(file.path)
+    var lang = path.basename(file.path, extname)
     var json = JSON.parse(file.contents.toString())
     var result = {}
     Object.keys(description).forEach(function (key) {
-      result[key] = json[key] || description[key] || ''
+      if (lang === descriptionAs) {
+        result[key] = json[key] || description[key] || ''
+      } else {
+        result[key] = json[key] || ''
+      }
     })
 
     file.contents = new Buffer(JSON.stringify(result, null, 2))
