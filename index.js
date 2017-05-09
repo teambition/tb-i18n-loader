2c42650f9e9c9d3a52cda217c3cd716401c26eef
LongYinan <lynweklm@gmail.com>
2017-05-09 10:24:44 +0800

    fix config

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 var loaderUtils = require('loader-utils')
 var util = require('./util')
 
-var languages = ['zh', 'zh_tw', 'en', 'ja', 'ko']
+var configLanguages = ['zh', 'zh_tw', 'en', 'ja', 'ko']
 
 module.exports = function (content) {
   if (this.cacheable) this.cacheable()
 
   var query = loaderUtils.parseQuery(this.query)
   var descriptionAs = query.descriptionAs
-  var languages = query.languages || config.languages
+  var languages = query.languages || configLanguages
   var description = util.parseDescription(content)
 
   var results = ['var i18n = require(\'tb-i18n\');']
